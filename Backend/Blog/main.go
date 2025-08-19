4ea5246effdaecb5035943ded261378331189052
Isidora Pavlovic <isidorapavlovic0@gmail.com>
2025-08-19 20:59:45 +0200

    Init blog service

---
+++ Backend/Blog/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using default values.")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://localhost:27017"
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer client.Disconnect(ctx)
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	log.Println("Successfully connected to MongoDB.")
+
+	dbName := os.Getenv("MONGO_DB_NAME")
+	if dbName == "" {
+		dbName = "blog_db"
+	}
+	collectionName := os.Getenv("MONGO_COLLECTION_NAME")
+	if collectionName == "" {
+		collectionName = "blog"
+	}
+
+}
