d52d074db2f25cfbc0fe1b4008c0ad1bdd89c209
lnosovic <lnosovic46@gmail.com>
2025-09-02 11:51:03 +0200

    Added auth for functions

--- Backend/Stakeholders/handler/ProfileHandler.go
+++ Backend/Stakeholders/handler/ProfileHandler.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/pavlovicisidora/soa-team7/auth"
 	"github.com/pavlovicisidora/soa-team7/service"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -14,6 +16,19 @@ type ProfileHandler struct {
 }
 
 func (handler *ProfileHandler) FindByUserId(writer http.ResponseWriter, req *http.Request) {
+	tokenStr := req.Header.Get("Authorization") // "Bearer <token>"
+	tokenStr = strings.TrimPrefix(tokenStr, "Bearer ")
+
+	claims, err := auth.VerifyJWT(tokenStr)
+	if err != nil {
+		http.Error(writer, "Unathorized"+err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	if claims.Role != "TURISTA" && claims.Role != "VODIC" {
+		http.Error(writer, "Forbidden: only TURISTA AND VODIC can see user profile", http.StatusForbidden)
+		return
+	}
 	userIdStr := mux.Vars(req)["userId"]
 	userId, err := primitive.ObjectIDFromHex(userIdStr)
 	if err != nil {
