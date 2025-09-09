426f7d42a891b91c41205ff5dabae4faf3ce987b
Iva Zivanovic <ivazivanovic02@gmail.com>
2025-09-09 20:37:59 +0200

    Fixed Srki's merged succesfully

--- Backend/Follower/service/follow_service.go
+++ Backend/Follower/service/follow_service.go
@@ -2,33 +2,33 @@ package service
 
 import (
 	"context"
-	"errors" // Potrebno za definisanje custom grešaka
+	"errors" 
 
 	"github.com/pavlovicisidora/soa-team7/Backend/Follower/model"
 	"github.com/pavlovicisidora/soa-team7/Backend/Follower/repo"
 )
 
-// Definišemo custom grešku za lakše rukovanje u handleru.
+
 var ErrCannotFollowSelf = errors.New("user cannot follow themselves")
 
-// UserService definiše interfejs za poslovnu logiku.
+
 type FollowService interface {
 	FollowUser(ctx context.Context, followerId string, followedId string) error
 	GetFollowing(ctx context.Context, followerId string) ([]*model.User, error)
 	GetFollowRecommendations(ctx context.Context, userId string) ([]*model.User, error)
 }
 
-// userService je implementacija interfejsa.
+
 type followService struct {
 	repo repo.FollowRepository
 }
 
-// NewUserService kreira novu instancu servisa.
+
 func NewFollowService(repo repo.FollowRepository) FollowService {
 	return &followService{repo: repo}
 }
 
-// FollowUser sadrži logiku za praćenje korisnika.
+
 func (s *followService) FollowUser(ctx context.Context, followerId string, followedId string) error {
 
 	if followerId == followedId {
@@ -39,8 +39,6 @@ func (s *followService) FollowUser(ctx context.Context, followerId string, follo
 }
 
 func (s *followService) GetFollowing(ctx context.Context, followerId string) ([]*model.User, error) {
-	// Samo prosleđujemo poziv repozitorijumu.
-	// Potpis metode je sada usklađen sa interfejsom i repozitorijumom.
 	return s.repo.GetFollowing(ctx, followerId)
 }
 
